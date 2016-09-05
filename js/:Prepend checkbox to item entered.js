f20d51a32cbf5820fe2f7436b9e30f0282cbfe20
Tiana Tucker <tianatucker@gmail.com>
2016-09-05 17:11:23 -0400

    add to list js and icon css

---
+++ js/:Prepend checkbox to item entered.js
@@ -0,0 +1,9 @@
+//On click add text input to list
+$(document).ready(function(){
+$('#add-btn').click(function(){
+	$('.incomplete').prepend($('<li>', {
+		text: $('#add-item').val()
+	}))
+})
+});
+
