1e3d27ccb4a5ed0dd77c0200d2f0a34a14806158
Tiana Tucker <tianatucker@gmail.com>
2016-09-17 14:43:23 -0400

    changed script order and clean up

--- js/app.js
+++ js/app.js
@@ -1,17 +1,8 @@
 //On click add text input to list
-/*$(document).ready(function(){
-  $('#add-btn').click(function(){
-	$('.incomplete').prepend($('<li>', {
-		text: $('#add-item').val()
-	}));
-});
-
-});*/
-
 $(document).ready(function(){
 $('#add-btn').click(function(){
 	$('.incomplete').prepend($('<li>', {
 		text: $('#add-item').val()
 	}))
-})
+});
 });
