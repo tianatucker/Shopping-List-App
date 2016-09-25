7e4a483e7f4df6d7b7786d0fd65012eb0e3d0175
Tiana Tucker <tianatucker@gmail.com>
2016-09-25 16:55:52 -0400

    add mentor notes and remove duplicate file

--- js/:Prepend checkbox to item entered.js
+++
@@ -1,9 +0,0 @@
-//On click add text input to list
-$(document).ready(function(){
-$('#add-btn').click(function(){
-	$('.incomplete').prepend($('<li>', {
-		text: $('#add-item').val()
-	}))
-})
-});
-
--- js/app.js
+++ js/app.js
@@ -5,4 +5,38 @@ $('#add-btn').click(function(){
 		text: $('#add-item').val()
 	}))
 });
+
+//Check for new DOM elements and apply checkmark on click event
+$('.incomplete').bind('DOMNodeInsertedIntoDocument',function(){
+    $('li').on('click', function() {
+    $(this).toggleClass('special');
 });
+});
+
+//When a unchecked box is clicked replace it with a checkmark
+$('li').on('click', function() {
+    $(this).toggleClass('special');
+});
+});
+
+//Check for new DOM elements and apply checkmark on click event
+$('.incomplete').bind('DOMNodeInsertedIntoDocument',function(){
+    $('li').on('click', function() {
+    $(this).toggleClass('special');
+});
+});
+
+//Show the delete button when rolling over a list item
+
+//Remove list item when the delete button is clicked
+
+
+//Notes with Nick
+
+//Maybe connect all button clicks to this
+
+//Jquery delagate for parent element's children, so instead process at the parent level then delegate the to correct child
+
+//Or find an li with a search string that was closest(surrounding/incompasses) to my mouse pointer. Event handler needed
+
+
