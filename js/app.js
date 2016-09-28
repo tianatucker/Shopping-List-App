85cdfa4af0499eb6014dee12db256e0f79d62fc2
Tiana Tucker <tianatucker@gmail.com>
2016-09-27 22:11:35 -0400

    resolved DOM addition interactivity issue and typo in html

--- js/app.js
+++ js/app.js
@@ -6,7 +6,13 @@ $('#add-btn').click(function(){
 	}))
 });
 
-//Check for new DOM elements and apply checkmark on click event
+//When a unchecked box is clicked replace it with a checkmark
+$('ul').on('click', 'li', function() {
+    $(this).toggleClass('special');
+  });
+});
+
+/*Check for new DOM elements and apply checkmark on click event
 $('.incomplete').bind('DOMNodeInsertedIntoDocument',function(){
     $('li').on('click', function() {
     $(this).toggleClass('special');
@@ -19,17 +25,15 @@ $('li').on('click', function() {
 });
 });
 
-//Check for new DOM elements and apply checkmark on click event
-$('.incomplete').bind('DOMNodeInsertedIntoDocument',function(){
-    $('li').on('click', function() {
-    $(this).toggleClass('special');
-});
-});
+/*$('.incomplete').on('DOMNodeInserted', function(e){
+  if ($(e.target).is('.MyClass')) {
+       DoSomething($(e.target));
+    }
+});*/
 
 //Show the delete button when rolling over a list item
 
 //Remove list item when the delete button is clicked
-
 
 //Notes with Nick
 
@@ -39,4 +43,4 @@ $('.incomplete').bind('DOMNodeInsertedIntoDocument',function(){
 
 //Or find an li with a search string that was closest(surrounding/incompasses) to my mouse pointer. Event handler needed
 
-
+//Only need one click (handler) event on the li
