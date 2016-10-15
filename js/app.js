219160ecb8ef2b506d17a148e52a3a890a1e19da
Tiana Tucker <tianatucker@gmail.com>
2016-10-15 16:57:47 -0400

    Add button on rollover state

--- js/app.js
+++ js/app.js
@@ -2,15 +2,35 @@
 $(document).ready(function(){
 $('#add-btn').click(function(){
 	$('.incomplete').prepend($('<li>', {
-		text: $('#add-item').val()
-	}))
-});
+		'text': $('#add-item').val()
+  }));
+$('ul').one('mouseover', 'li', function() {
+    $(this).append('<input type="button" value="Delete" class="delete-btn">');
+  });
 
 //When a unchecked box is clicked replace it with a checkmark
 $('ul').on('click', 'li', function() {
     $(this).toggleClass('special');
   });
 });
+});
+/*
+$('li').on('hover', function() {
+    $(this).add("<input>").appendTo(".incomplete");
+  });
+});
+
+$('ul').mouseover('li', function(){
+  $('.delete-btn').css('display','inline');
+});
+
+$('ul').hover('li', function() {
+    $(this).after('<input type="button" value="Delete" class="delete-btn" />');
+  });*/
+
+//Show the delete button when rolling over a list item
+
+//Remove list item when the delete button is clicked
 
 /*Check for new DOM elements and apply checkmark on click event
 $('.incomplete').bind('DOMNodeInsertedIntoDocument',function(){
@@ -31,10 +51,6 @@ $('li').on('click', function() {
     }
 });*/
 
-//Show the delete button when rolling over a list item
-
-//Remove list item when the delete button is clicked
-
 //Notes with Nick
 
 //Maybe connect all button clicks to this
@@ -44,3 +60,8 @@ $('li').on('click', function() {
 //Or find an li with a search string that was closest(surrounding/incompasses) to my mouse pointer. Event handler needed
 
 //Only need one click (handler) event on the li
+
+//Have to close things in reverse order
+
+//If it's a
+
